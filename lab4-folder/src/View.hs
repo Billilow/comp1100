ed1784985308ed9a2f6045ec599dc2a2fc265e0c
u7527005 <u7527005@anu.edu.au>
2023-03-14 13:42:34 +1100

    roatation fixed

--- lab4-folder/src/View.hs
+++ lab4-folder/src/View.hs
@@ -14,7 +14,7 @@ myPicture :: Picture
 --myPicture = coordinatePlane & myRectangle
 -- e3
 -- myPicture = coordinatePlane & translated 3 (-2) myRectangle'
-myPicture = myLambda
+myPicture = colouredSquares'
 
 
 -- For each exercise, create a new function below so that we can see you have
@@ -40,7 +40,7 @@ colouredSquares = coloured blue myRectangle' & translated 0 3 (coloured orange m
 
 --e8
 colouredSquares' ::Picture
-colouredSquares' = rotated 45 (coloured blue myRectangle') & rotated 45 (translated 0 3 (coloured orange myRectangle')) & rotated 45 (translated 3 3 (coloured green myRectangle')) & rotated 45 (translated 3 0 (coloured yellow myRectangle'))
+colouredSquares' = rotated (pi/4) (coloured blue myRectangle') & rotated (pi/4) (translated 0 3 (coloured orange myRectangle')) & rotated (pi/4) (translated 3 3 (coloured green myRectangle')) & rotated (pi/4) (translated 3 0 (coloured yellow myRectangle'))
 
 --e9
 myLambda = coordinatePlane & thickPolyline 0.7 [(3,7), (7,1), (5, 4), (3,1)]
