45a38aaec26266ee767bb1a1d93626cee94cd515
u7527005 <u7527005@anu.edu.au>
2023-03-14 13:36:21 +1100

    exercise 9 drawing lambda

--- lab4-folder/src/View.hs
+++ lab4-folder/src/View.hs
@@ -8,30 +8,42 @@ import CodeWorld
 -- This function can be assigned to different pictures
 -- (e.g. myRectangle, myRectangle', ...)
 myPicture :: Picture
+--e1
+--myPicture = coordinatePlane
+--e2
+--myPicture = coordinatePlane & myRectangle
+-- e3
 -- myPicture = coordinatePlane & translated 3 (-2) myRectangle'
-myPicture = colouredSquares'
+myPicture = myLambda
 
 
 -- For each exercise, create a new function below so that we can see you have
 -- completed each exercise
 -- (e.g. myRectangle, myRectangle', colouredRectangle, colouredSquares, ...)
 
+--e4
 myRectangle :: Picture
 myRectangle = rectangle 2 2
 
+--e5
 myRectangle' :: Picture
 myRectangle' = solidRectangle 2 2
 
+--e6
 colouredRectangle :: Picture
 colouredRectangle = coloured blue myRectangle'
 
+
+--e7
 colouredSquares :: Picture 
 colouredSquares = coloured blue myRectangle' & translated 0 3 (coloured orange myRectangle') & translated 3 3 (coloured green myRectangle') & translated 3 0 (coloured yellow myRectangle')
 
+--e8
 colouredSquares' ::Picture
 colouredSquares' = rotated 45 (coloured blue myRectangle') & rotated 45 (translated 0 3 (coloured orange myRectangle')) & rotated 45 (translated 3 3 (coloured green myRectangle')) & rotated 45 (translated 3 0 (coloured yellow myRectangle'))
 
-
+--e9
+myLambda = coordinatePlane & thickPolyline 0.7 [(3,7), (7,1), (5, 4), (3,1)]
 ------------------------------------------------------------------------------
 --  THIS SECTION BELOW REQUIRED FOR COMP1130 STUDENTS, OPTIONAL FOR 1100.   --
 ------------------------------------------------------------------------------
